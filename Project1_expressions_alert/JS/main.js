370b304b7d12d60ab8719fad38c89458b56720b7
Jenna Follin <140860498+jleef5046@users.noreply.github.com>
2023-11-10 11:11:06 -0800

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"
+
+document.write(Sent1 + Sent2) //This represents concatanating two string variables together
+
+window.alert(10+17) //This is an expression that will be displayed in a pop up box
