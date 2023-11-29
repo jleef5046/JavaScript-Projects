10724db99935ec6c05170f4e2eab3cdcd54dcdd1
Jenna Follin <140860498+jleef5046@users.noreply.github.com>
2023-11-29 15:35:36 -0800

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,18 @@
+document.write(typeof 53); //Demostrates the use of the "typeof" operator
+
+document.write("Oregon Est." + 1859); //Demonstrates how to combine an string and number
+
+
+document.write(10 == 15); //Demonstrates the use of == operator
+
+A = 23
+B = 22
+document.write(A === B); //Demonstrates the use of === operator
+
+document.write(23 < 37 && 12 < 15); //Demonstrates the use of && operator
+
+document.write(5 > 10 || 10 > 4); //Demonstrates the use of || operator
+
+function Not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10); //Demonstrates the use of ! "Not" operator
+}
